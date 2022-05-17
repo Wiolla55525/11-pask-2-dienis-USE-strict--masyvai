2112558edd05794a87a92b4dcaa492a7c240026f
Violeta Vinogrodska <105384019+Wiolla55525@users.noreply.github.com>
2022-05-17 19:06:23 +0300

    commit

---
+++ javascr.js
@@ -0,0 +1,11 @@
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6];
+
+let combinedArrau = array1.concat(array2);
+
+console.log(array1)
+console.log(combinedArrau);
+
+for(let i = 0; i<10; i++) {
+    console.log(i);
+}
